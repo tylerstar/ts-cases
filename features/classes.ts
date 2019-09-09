c47b76ccd6f33754b335b4f3a78c6edfae5081d1
Tyler Leung <binglux@gmail.com>
2019-09-10 00:31:39 +0800

    feat(tyepscript-syntax): classes, tuples and interfaces

---
+++ features/classes.ts
@@ -0,0 +1,31 @@
+class Vehicle {
+  constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log("beep");
+  }
+}
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
+
+const car = new Car(4, "red");
+car.startDrivingProcess();
+console.log(car.color);
+
