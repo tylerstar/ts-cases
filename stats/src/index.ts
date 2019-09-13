08d302879ad45fe957b4090e7b3ceace044478e2
Tyler Leung <binglux@gmail.com>
2019-09-14 06:11:51 +0800

    refactor(file-reader): use abstract class to refactor file reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,23 +1,12 @@
-import fs from "fs";
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
-
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D"
-}
+const reader = new MatchReader("football.csv");
+reader.read();
 
 let manUnitedWins = 0;
 
-for (const match of matches) {
+for (const match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
