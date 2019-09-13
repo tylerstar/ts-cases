10f6b1c5b6a9858aaf8fd2579f957718bb2ff968
Tyler Leung <binglux@gmail.com>
2019-09-14 04:51:23 +0800

    refactor(interface->inheritence): use abstract class to replace sortable interface so sort() method can attach to all data structures

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,19 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(sorter.collection);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
