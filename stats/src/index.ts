bd9d2f8abb6ea0454feaeb2af8000c733f342a27
Tyler Leung <binglux@gmail.com>
2019-09-14 05:10:28 +0800

    refactor(magical string & enum): use variables to replace magical strings and use a enum to type the strings

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -9,12 +9,18 @@ const matches = fs
     return row.split(",");
   });
 
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
 let manUnitedWins = 0;
 
 for (const match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
