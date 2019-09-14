006ff0b784173ab2c8f178ae972c08fbc637dfd8
Tyler Leung <binglux@gmail.com>
2019-09-15 03:10:26 +0800

    feat(matchReader): improve match-reader's interface with static method

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,10 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CsvFileReader_1 = require("./CsvFileReader");
 var Summary_1 = require("./Summary");
-var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
-matchReader.load();
+var matchReader = MatchReader_1.MatchReader.fromCsv("football.csv");
 var summary = Summary_1.Summary.winAnalysisWithHtmlReport("Man United");
+matchReader.load();
 summary.buildAndReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,8 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { WinAnalysis } from "./analyzers/WinAnalysis";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-const csvFileReader = new CsvFileReader("football.csv");
-
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+const matchReader = MatchReader.fromCsv("football.csv");
 const summary = Summary.winAnalysisWithHtmlReport("Man United");
+
+matchReader.load();
 summary.buildAndReport(matchReader.matches);
